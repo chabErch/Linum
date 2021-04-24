fdbfc3fa87f66a4d11e7db2d00eeebf0d4c17472
ChabErch <chaberch@yandex.ru>
2021-04-24 14:07:21 +0300

    dring example

---
+++ examples/char_painter.py
@@ -0,0 +1,36 @@
+from datetime import date
+
+from linum.char_painter import CharPainter
+from linum.context import Context
+from linum.task import Task
+
+# Add some tasks
+tasks = [
+    Task('Vacation', date(2020, 1, 2), 20),
+
+    Task('Duty', date(2020, 2, 1), 2),
+    Task('Duty', date(2020, 2, 7), 2),
+    Task('Duty', date(2020, 2, 15), 2),
+    Task('Duty', date(2020, 2, 25), 2),
+
+    Task('Task #1', date(2020, 1, 22), 10),
+    Task('Task #2', date(2020, 2, 3), 10),
+    Task('Task #3', date(2020, 2, 23), 10),
+    Task('Task #4', date(2020, 3, 17), 10),
+
+    Task('Current day task', length=5)
+]
+
+# Initial context
+c = Context()
+
+# Tasks for week
+cp = CharPainter(tasks, c)
+print(cp.render())
+
+# Tasks for 4 months
+c.start_date = date(2020, 1, 1)
+c.finish_date = date(2020, 5, 1)
+cp = CharPainter(tasks, c)
+print(cp.render())
+
