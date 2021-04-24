8d950358ca08ef9e1d7e589061befa5686d2c8f1
ChabErch <chaberch@yandex.ru>
2021-04-24 14:21:34 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Linum',
+    version='0.1',
+    packages=['linum'],
+    url='https://github.com/chabErch/Linum',
+    license='MIT',
+    author='chaberch',
+    author_email='chaberch@yandex.ru',
+    description='The tool for tasks visualization — like Gantt chart, but compact.'
+)
