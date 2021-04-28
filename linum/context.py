ab6015af00a99e1b6d311db5182cf59c156476b3
ChabErch <chaberch@yandex.ru>
2021-04-28 23:21:28 +0300

    add special context class

--- linum/context.py
+++ linum/context.py
@@ -4,9 +4,8 @@ from datetime import date, timedelta
 class Context:
 
     def __init__(self, **kwargs):
-        self.cell_width = 3
-        self.start_date = date.today()
-        self.finish_date = date.today() + timedelta(7)
+        self.start = date.today()
+        self.finish = date.today() + timedelta(7)
         self.inner_borders = True
         self.month_inner_borders = True
         self.left_border = True
@@ -18,11 +17,11 @@ class Context:
 
     @property
     def length(self):
-        return (self.finish_date - self.start_date).days
+        return (self.finish - self.start).days
 
     @length.setter
     def length(self, length: int):
-        self.finish_date = self.start_date + timedelta(max(length, 0))
+        self.finish = self.start + timedelta(max(length, 0))
 
     @property
     def year(self) -> None:
@@ -30,5 +29,12 @@ class Context:
 
     @year.setter
     def year(self, year: int):
-        self.start_date = date(year, 1, 1)
-        self.finish_date = date(year + 1, 1, 1)
+        self.start = date(year, 1, 1)
+        self.finish = date(year + 1, 1, 1)
+
+
+class CharPainterContext(Context):
+
+    def __init__(self, **kwargs):
+        self.cell_width = 3
+        super().__init__(**kwargs)
