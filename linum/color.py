b9265c19f2eb2f4280a5b8f63f7a0ba00b9eef7a
ChabErch <chaberch@yandex.ru>
2021-05-08 20:21:28 +0300

    add get_random_rgb and apply_blackout methods

--- linum/color.py
+++ linum/color.py
@@ -1,12 +1,17 @@
 import colorsys
+import random
 import re
-from typing import Union, Tuple
+from typing import Union, Tuple, Set, Dict
+
 import wcag_contrast_ratio as contrast
+
+from linum.styles import color_palettes
 
 
 class Color:
+    used_colors: Dict[str, Set[int]] = {}
 
-    def __init__(self, rgb: Union[str, int] = 0x000000):
+    def __init__(self, rgb: Union[str, int, 'Color'] = 0x000000):
         if isinstance(rgb, int):
             self.r = (rgb & 0xFF0000) >> 16
             self.g = (rgb & 0x00FF00) >> 8
@@ -15,11 +20,16 @@ class Color:
             self.r = int(rgb[1:3], 16)
             self.g = int(rgb[3:5], 16)
             self.b = int(rgb[5:7], 16)
+        elif isinstance(rgb, Color):
+            self.r, self.g, self.b = rgb.r_g_b
         else:
             raise ValueError
 
     def __str__(self):
         return '#' + hex(self.rgb)[2:].zfill(6)
+
+    def __copy__(self):
+        return Color(self.rgb)
 
     def contrast(self, color: 'Color') -> float:
         return contrast.rgb(self.r_g_b_percents, color.r_g_b_percents)
@@ -67,3 +77,21 @@ class Color:
         g = g << 8
         return Color(r + g + b)
 
+    @staticmethod
+    def get_random_rgb(palette: str = "material_design") -> int:
+        colors = color_palettes.palettes[palette]
+        palette_colors = set(colors.values())
+        used_colors = Color.used_colors.setdefault(palette, set())
+        difference = palette_colors.difference(used_colors)
+        if not difference:
+            Color.used_colors[palette] = set()
+            difference = palette_colors
+        color = random.choice(list(difference))
+        Color.used_colors[palette].add(color)
+        return color
+
+    def apply_blackout(self, blackout_value: float):
+        h, s, v = self.h_s_v_percents
+        v = max(0.0, v - blackout_value)
+        self.r, self.g, self.b = self.from_h_s_v_percents(h, s, v).r_g_b
+        return Color(self.rgb)
