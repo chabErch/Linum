38da87f4e57a4ae3b19d459a76e2f24ba353d245
ChabErch <chaberch@yandex.ru>
2021-05-08 20:24:56 +0300

    add layer list rendering

--- linum/excel_renderer/calendar/calendar.py
+++ linum/excel_renderer/calendar/calendar.py
@@ -4,6 +4,9 @@ from xlsxwriter.worksheet import Worksheet
 from linum import LayerList
 from linum.context import ExcelRendererContext
 from linum.excel_renderer.calendar.header.header import Header
+from linum.excel_renderer.calendar.space.space_row import SpaceRow
+from linum.excel_renderer.calendar.views.layer_list_view import LayerListView
+from linum.helper import split_by_months
 
 
 class Calendar:
@@ -16,8 +19,46 @@ class Calendar:
         days_off = self.context.days_off
         workdays = self.context.workdays
 
-        header_style = self.context.styles.get_sub_style("header")
-        days_off_header_style = self.context.styles.get_sub_style("days_off").get_sub_style("header")
-        header = Header(self.context.start, self.context.length, days_off, workdays,
-                        header_style, days_off_header_style)
-        header.render(row, column, worksheet, workbook)
+        # Base styles
+        styles = self.context.styles
+        days_off_styles = styles.get_sub_style("days_off")
+
+        # Header styles
+        header_style = styles.get_sub_style("header")
+        days_off_header_style = days_off_styles.get_sub_style("header")
+
+        # Layer styles
+        layers_style = styles.get_sub_style("layers")
+        days_off_layers_style = days_off_styles.get_sub_style("layers")
+
+        # Space row styles
+        space_row_style = layers_style.get_sub_style("space_row").get_sub_style("_")
+        days_off_space_row_style = days_off_layers_style.get_sub_style("space_row").get_sub_style("_")
+        days_off_space_row_style.parents.insert(0, space_row_style)
+
+        # Splitting by periods
+        row_offset = 0
+        months = split_by_months(self.context.start, self.context.length)
+        for i in range(0, len(months), self.context.months_in_row):
+            m = months[i:i + self.context.months_in_row]
+            d, _ = m[0]
+            days = sum([d for _, d in m])
+
+            # Rendering header
+            header = Header(d, days, days_off, workdays,
+                            header_style, days_off_header_style)
+            header.render(row + row_offset, column, worksheet, workbook)
+
+            # Rendering space row
+            sr = SpaceRow(d, days, days_off, workdays,
+                          space_row_style, days_off_space_row_style)
+            sr.render(row + row_offset + 3, column, worksheet, workbook)
+
+            # Rendering layer list
+            llv = LayerListView(self.layer_list, d, days,
+                                layers_style, days_off_layers_style,
+                                days_off, workdays)
+            offset = llv.render(row + row_offset + 4, column, worksheet, workbook)
+
+            # Calculating offset
+            row_offset = row_offset + 3 + offset + 1
