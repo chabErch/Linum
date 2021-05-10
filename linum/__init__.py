f4f981d1e00a0223315663f13812292c679b87f6
ChabErch <chaberch@yandex.ru>
2021-05-10 21:12:49 +0300

    add public classes

--- linum/__init__.py
+++ linum/__init__.py
@@ -1,7 +1,10 @@
+from excel_renderer.excel_renderer import ExcelRenderer
+from txt_renderer.console_renderer import ConsoleRenderer
+from txt_renderer.txt_renderer import TxtRenderer
 from .composer import Composer
 from .context import Context, TxtRendererContext
 from .layer import Layer
 from .layer_list import LayerList
-from .task import Task
 from .loader import Loader
+from .task import Task
 from .task_part import TaskPart
