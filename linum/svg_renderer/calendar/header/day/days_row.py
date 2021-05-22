a5f5b7725dac15b7154ac1ef54fd61271bb1ff1f
ChabErch <chaberch@yandex.ru>
2021-05-22 23:29:53 +0300

    add svg day

---
+++ linum/svg_renderer/calendar/header/day/days_row.py
@@ -0,0 +1,44 @@
+from datetime import date, timedelta
+from typing import List, Optional
+
+from linum.helper import is_day_off
+from linum.svg_renderer.base.row import Row
+from linum.svg_renderer.base.style import Style
+from linum.svg_renderer.calendar.header.day.day_cell import DayCell
+from linum.svg_renderer.calendar.header.day.day_off_day_cell import DayOffDayCell
+from linum.svg_renderer.calendar.header.day.workday_day_cell import WorkdayDayCell
+
+
+class DaysRow(Row):
+
+    def __init__(self, start: date, length: int, width: float,
+                 workday_style: Optional[Style] = None, day_off_style: Optional[Style] = None,
+                 workdays: Optional[List[date]] = None, days_off: Optional[List[date]] = None):
+        self.start = start
+        self.length = length
+        self.width = width
+
+        self.workday_style = workday_style or Style('default day')
+        self.day_off_style = day_off_style or Style('default day-off day')
+
+        self.workdays = workdays or []
+        self.days_off = days_off or []
+
+    @property
+    def height(self) -> int:
+        wd_height = self.workday_style.get("height", 100)
+        do_height = self.day_off_style.get("height", 100)
+        return max(wd_height, do_height)
+
+    @property
+    def cells(self) -> List[DayCell]:
+        cells = []
+        cell_width = self.width / self.length
+        for i in range(self.length):
+            d = self.start + timedelta(i)
+            if is_day_off(d, self.days_off, self.workdays):
+                cell = DayOffDayCell(d, cell_width, self.day_off_style)
+            else:
+                cell = WorkdayDayCell(d, cell_width, self.workday_style)
+            cells.append(cell)
+        return cells
