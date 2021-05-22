969c2ad19308d5113314da9a71cf63dcc9f6a0a8
ChabErch <chaberch@yandex.ru>
2021-05-22 23:29:41 +0300

    add svg weekday

---
+++ linum/svg_renderer/calendar/header/weekday/workday_weekday_cell.py
@@ -0,0 +1,16 @@
+from datetime import date
+from typing import Optional
+
+from linum.svg_renderer.base.style import Style
+from linum.svg_renderer.calendar.header.weekday.weekday_cell import WeekdayCell
+
+
+class WorkdayWeekdayCell(WeekdayCell):
+
+    def __init__(self, date_: date, width: float,
+                 style: Optional[Style] = None):
+        super().__init__(date_, width, style)
+
+    @classmethod
+    def get_class(cls):
+        return "workday"
