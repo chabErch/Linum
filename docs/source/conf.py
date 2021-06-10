82caf9f24e402224d4fbf7d617b7a35cb4900041
ChabErch <chaberch@yandex.ru>
2021-06-10 11:38:59 +0300

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, chaberch'
 author = 'chaberch'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9'
+release = '0.9.2'
 
 # -- General configuration ---------------------------------------------------
 
